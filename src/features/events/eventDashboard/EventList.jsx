3446f5b2c93875cf5d30ec13ff22dd6c3fd24d47
mattespositotech <matt@teamduality.dev>
2023-09-22 13:24:02 -0500

    End of Section 4

--- src/features/events/eventDashboard/EventList.jsx
+++ src/features/events/eventDashboard/EventList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import EventListItem from "./EventListItem";
 
-export default function EventList({events}) {
-    return (
-        <div>
-            {events.map(event => (
-                <EventListItem event={event} key={event.id}/>
-            ))}
-        </div>
-        
-    )
+export default function EventList({ events, selectEvent, deleteEvent }) {
+  return (
+    <div>
+      {events.map((event) => (
+        <EventListItem
+          event={event}
+          key={event.id}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
+      ))}
+    </div>
+  );
 }
