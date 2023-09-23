26bfff1d1ac90cb93c53d2da5992d134ad53af7b
mattespositotech <matt@teamduality.dev>
2023-09-23 11:19:33 -0500

    End of Section 5

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
+import { Link } from "react-router-dom";
 
 export default function EventListItem({ event, selectEvent, deleteEvent }) {
   return (
@@ -38,7 +39,8 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
           content="Delete"
         />
         <Button
-          onClick={() => selectEvent(event)}
+          as={Link}
+          to={`/events/${event.id}`}
           color="teal"
           floated="right"
           content="View"
