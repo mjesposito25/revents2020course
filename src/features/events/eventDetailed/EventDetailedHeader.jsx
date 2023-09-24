a2230baa99da7d4c771adf203141d2a37e6fea9d
mattespositotech <matt@teamduality.dev>
2023-09-24 17:46:23 -0500

    End of Section 6

--- src/features/events/eventDetailed/EventDetailedHeader.jsx
+++ src/features/events/eventDetailed/EventDetailedHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
 
-export default function EventDetailedHeader() {
+export default function EventDetailedHeader({event}) {
   const eventImageStyle = {
     filter: "brightness(30%)",
   };
@@ -19,7 +19,7 @@ export default function EventDetailedHeader() {
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
-        <Image src={`/assets/categoryImages/drinks.jpg`} fluid style={eventImageStyle} />
+        <Image src={`/assets/categoryImages/${event.category}.jpg`} fluid style={eventImageStyle} />
 
         <Segment basic style={eventImageTextStyle}>
           <Item.Group>
@@ -27,12 +27,12 @@ export default function EventDetailedHeader() {
               <Item.Content>
                 <Header
                   size="huge"
-                  content="Event Title"
+                  content={event.title}
                   style={{ color: "white" }}
                 />
-                <p>Event Date</p>
+                <p>{event.date}</p>
                 <p>
-                  Hosted by <strong>Bob</strong>
+                  Hosted by <strong>{event.hostedBy}</strong>
                 </p>
               </Item.Content>
             </Item>
@@ -44,7 +44,7 @@ export default function EventDetailedHeader() {
         <Button>Cancel My Place</Button>
         <Button color="teal">JOIN THIS EVENT</Button>
 
-        <Button as={Link} to={`/manage/`} color="orange" floated="right">
+        <Button as={Link} to={`/manage/${event.id}`} color="orange" floated="right">
           Manage Event
         </Button>
       </Segment>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
 //import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/layout/ScrollToTop";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+
+const rootEl = document.getElementById("root");
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 }
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', function () {
+  module.hot.accept("./app/layout/App", function () {
     setTimeout(render);
   });
 }
